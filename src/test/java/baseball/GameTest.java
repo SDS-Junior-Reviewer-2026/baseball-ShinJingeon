74042673aed0a403bb387763d1746999d04d335e
jingeon <sjg729729@gmail.com>
2026-04-03 13:32:14 +0900

    [refactoring] 36p

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,20 +1,27 @@
 package baseball;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
 
+	private Game game;
+
+	@BeforeEach
+	void setUp() {
+		game = new Game();
+	}
+
 	@Test
 	void createGame() {
-		Game game = new Game();
+		game = new Game();
 		assertNotNull(game);
 	}
 
 	@Test
 	void throwExceptionWhenInputIsNull() {
 		assertThrows(IllegalArgumentException.class, () -> {
-			Game game = new Game();
 			game.guess(null);
 		});
 	}
